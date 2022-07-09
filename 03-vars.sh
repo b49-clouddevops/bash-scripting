eda53d6e0effb97a66e71f286444030b89928153
mvpc-11232 <mvp2612@hotmail.com>
2022-07-09 08:21:34 +0530

    This is my first commint

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,10 +6,16 @@ a=10
 # 10 s a string here 
 b=abc
 
+
 # abc is also a string 
 
 # When to use Double Quotes ??? When your input us having any special characters, then you need to enclose them in QUOTES.
 
 echo $a  # $a is printing the value of a 
 echo $b
+echo ${b}
 echo "The value of c is $c"
+
+# rm -rf /data/${DATA_DIR} ---> /data/
+
+# $a or  ${a}
