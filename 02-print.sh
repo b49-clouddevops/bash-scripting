3e1a00e197295eb9a34b62091737d5d917c83c65
mvpc-11232 <mvp2612@hotmail.com>
2022-07-09 08:14:38 +0530

    This is my first commint

--- 02-print.sh
+++ 02-print.sh
@@ -12,4 +12,4 @@ echo -e "\e[35m I am printing MAGNETA COLOR \e[0m "
 ## To have a background color
 # echo -e "\e[background; COL-CODEm  MESSAGE  \e[0m" 
 
-echo -e "\e[45;33m I am printing COLORS \e[0m "
+echo -e "\e[45;33m I am printing COLORS \e[0m"
---
+++ 03-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+### A Variable is something whichs holds the values dynamically.
+
+a=10 
+# 10 s a string here 
+b= abc 
+
+# abc is also a string 
+
+# When to use Double Quotes ??? When your input us having any special characters, then you need to enclose them in QUOTES.
+
+echo $a  # $a is printing the value of a 
+echo $b 
