df9fe3c86691905fc6716baa9ab3d1afd4f42efb
mvpc-11232 <mvp2612@hotmail.com>
2022-07-09 08:28:16 +0530

    This is my first commint

--- 03-vars.sh
+++ 03-vars.sh
@@ -5,6 +5,7 @@
 a=10 
 # 10 s a string here 
 b=abc
+d=300
 
 
 # abc is also a string 
@@ -21,3 +22,7 @@ echo Value of d is $d    ### Accessing a varianble from shell
 # rm -rf /data/${DATA_DIR} ---> /data/
 
 # $a or  ${a}
+
+# Varaibles that are declared inside the scripts are local variables 
+# Variables that are declared as shell using export are environment variables. When the values are not consistent and would based on the running, then go with env variables.
+
