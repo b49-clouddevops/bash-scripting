eb2d5de8120f27dd123567ac581fa221bddbfb63
mvpc-11232 <mvp2612@hotmail.com>
2022-07-09 08:41:36 +0530

    This is my first commint

---
+++ 05-special-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+
+# $0  : This will print the script name that you are running 
+# $?  : This will show your the status code of the last execution
+
+echo "Script we are executing is $0"
