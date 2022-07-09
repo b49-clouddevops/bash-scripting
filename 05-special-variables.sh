7bc40edc861bf15be2de091230254962157c8f52
mvpc-11232 <mvp2612@hotmail.com>
2022-07-09 08:44:03 +0530

    This is my first commint

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -1,7 +1,17 @@
 #!/bin/bash 
 
 
+### Special Variables 
 # $0  : This will print the script name that you are running 
 # $?  : This will show your the status code of the last execution
+# $0 to $n , $* , $@ , $# , $$
 
 echo "Script we are executing is $0"
+
+# Through command line you can pass up to 9 variables 
+
+echo "The value of X is $1"
+echo "Name of the Trainer is $2"
+
+# sh script.sh 10  20  30 
+#              $1 $2  $3
