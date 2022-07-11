63ca8b56a4e3173c9be127f766b0cba103199102
mvpc-11232 <mvp2612@hotmail.com>
2022-07-11 08:33:33 +0530

    Auto Push Commit

--- 12-if.sh
+++ 12-if.sh
@@ -3,6 +3,6 @@
 ACTION=$1 
 
 
-if [ "$ACTION" = "start" ] ; then 
-    echo -e "Selection option is \e[32m start \e[0m"
-fi 
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "Selection option is \e[32m start \e[0m"
+# fi 
