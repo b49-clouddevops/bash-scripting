9508369e0583ac79f58b4c080a6eba4daa7b39de
mvpc-11232 <mvp2612@hotmail.com>
2022-07-11 07:49:44 +0530

    Auto Push Commit

--- common.sh
+++ common.sh
@@ -4,4 +4,5 @@ common_fun() {
     echo "_________ Function Start ________"
     echo "My funciton name is common_fun"
     echo "I am called from common.sh"
+    echo "_________ Function Start ________"
 }
