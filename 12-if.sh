18bff5225ddbd8e09493ab7e2744e31b171d40be
mvpc-11232 <mvp2612@hotmail.com>
2022-07-11 08:36:37 +0530

    Auto Push Commit

--- 12-if.sh
+++ 12-if.sh
@@ -15,3 +15,10 @@ ACTION=$1
 # else 
 #     echo -e " \e[33m Valid option is start only \e[0m"
 # fi 
+
+# If Else 
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "Selected option is \e[32m start \e[0m"
+# else 
+#     echo -e " \e[33m Valid option is start only \e[0m"
+# fi 
