1b9d50b321f2b7c91c6c3eed7efce9f6e0c5a0e4
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:44:34 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,11 +6,12 @@ LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
 
 stat() {
-if [ $? -eq 0 ] ; then 
-    echo -e " \e[32m SUCCESS \e[0m"
-else 
-    echo -e " \e[31m FAILURE \e[0m"
-fi 
+    if [ $? -eq 0 ] ; then 
+        echo -e " \e[32m SUCCESS \e[0m"
+    else 
+        echo -e " \e[31m FAILURE \e[0m"
+    fi 
+}
 
 echo -n "Installing Nginx: "
 yum install nginx -y &>> $LOGFILE
