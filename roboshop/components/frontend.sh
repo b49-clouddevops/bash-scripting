0e4389f1d0b3c556ec928e0ae2cc817bdbd4ea21
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:45:23 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -26,6 +26,7 @@ stat $?
 
 echo -n "Downloading and extracting $COMPONEN: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
