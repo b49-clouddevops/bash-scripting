15a62106297cfe5d31c04c26a922aa07a6d6ac15
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:30:22 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,7 +3,7 @@ set -e
 
 USED_ID=$(id -u)
 if[ $USED_ID -ne 0  ] ; then  
-    echo -e "\e[31m You Need to run it as a root user only"
+    echo -e "\e[31m You need to run it as a root user only"
 fi 
 
 yum install nginx -y
