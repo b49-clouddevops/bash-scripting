88ddc168b04178bb29fac2542e903438e083b313
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 09:03:24 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -20,6 +20,8 @@ systemctl start mongod &>> $LOGFILE
 stat $?
 
 echo -n "Updating $COMPONENT Listenting address: "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat $? 
 
 echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
 
