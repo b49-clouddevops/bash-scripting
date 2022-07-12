c58f92021a5137dda4d8020644487f5d1b655431
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:43:25 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,6 +23,7 @@ else
     echo -e " \e[31m FAILURE \e[0m"
 fi 
 
+echo -n "Downloading and extracting $COMPINENT"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
