e0441b72c7ef195bc791d175584d76486a187ad4
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:54:46 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -10,3 +10,7 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 # systemctl enable mongod
 # systemctl start mongod
 
+1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+Config file:   `# vim /etc/mongod.conf`
+
