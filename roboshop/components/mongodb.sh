2277b381eca8d13f851cbd8298b0147a99d4d651
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:56:20 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -7,9 +7,9 @@ MONGODB_REPO_URL="https://raw.githubusercontent.com/stans-robot-project/mongodb/
 source components/common.sh 
 
 curl -s -o /etc/yum.repos.d/mongodb.repo $MONGODB_REPO_URL
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
 
 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
 
