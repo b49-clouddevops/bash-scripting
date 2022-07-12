83d33a752a6268405c65955cff0d42dcd5b25962
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:44:54 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,7 +6,7 @@ LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
 
 stat() {
-    if [ $? -eq 0 ] ; then 
+    if [ $1 -eq 0 ] ; then 
         echo -e " \e[32m SUCCESS \e[0m"
     else 
         echo -e " \e[31m FAILURE \e[0m"
