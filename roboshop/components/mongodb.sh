e9bbcf4b4fdfcefa684429f0204de64e378eb943
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 09:05:39 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -24,7 +24,8 @@ systemctl enable mongod  &>> $LOGFILE
 systemctl restart mongod &>> $LOGFILE 
 stat $?
 
-echo -n "Downloading the $COMPONENT schema""
+echo -n "Downloading the $COMPONENT schema: "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
 
