c1c6ccd24cfa80915e8f250aec6ff11ebc61950e
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:54:31 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -6,3 +6,7 @@ LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh 
 
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+
