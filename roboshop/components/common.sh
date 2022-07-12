23c5bd7da335f806f9b228b433a1e33d7c307a53
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:52:56 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -5,3 +5,11 @@ if [ $USER_ID -ne 0  ] ; then
     echo -e "\e[31m You need to run it as a root user only \e[0m"
     exit 1
 fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e " \e[32m SUCCESS \e[0m"
+    else 
+        echo -e " \e[31m FAILURE \e[0m"
+    fi 
+}
