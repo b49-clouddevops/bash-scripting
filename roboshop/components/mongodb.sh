adf5f9768d895f24592e0c8a9299d021410d8546
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 09:04:42 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -25,12 +25,6 @@ stat $?
 
 echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
 
-# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
-
-# Config file:   `# vim /etc/mongod.conf`
-
-#  systemctl restart mongod
-
 #  # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 # # cd /tmp
