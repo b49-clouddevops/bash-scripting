06bdec4694803e2f6fe6af0fa9f3df201e6de9a8
mvpc-11232 <mvp2612@hotmail.com>
2022-07-12 08:45:30 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,7 +24,7 @@ echo -n "Starting Nginx: "
 systemctl start nginx
 stat $?
 
-echo -n "Downloading and extracting $COMPONEN: "
+echo -n "Downloading $COMPONEN: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
