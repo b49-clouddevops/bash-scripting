1ebc00933bd79839fe7dd267df43f544659e1084
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:38:56 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -18,6 +18,7 @@ echo -n "Updating the $COMPONENT listenting address: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>> $LOGFILE 
 stat $? 
 
+# Calling start service function 
 START_SERVICE
 
 
