c612f79958a147015b51ad500ed21c8f8807cd2c
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:53:42 +0530

    Auto Push Commit

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e 
+COMPONENT=user 
+LOGFILE="/tmp/$COMPONENT.log"
+APPUSER="roboshop"
 
 source components/common.sh
 
-echo "Hello, I am cart"
+# Calling NodeJS Function
+NODEJS 
+
+echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
+
