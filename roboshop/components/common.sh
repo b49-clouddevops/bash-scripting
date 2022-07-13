d09c9b6dc33c6233c4e24008ce52f0cc3549892d
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:15:03 +0530

    Auto Push Commit

---
+++ roboshop/components/common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0  ] ; then  
+    echo -e "\e[31m You need to run it as a root user only \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e " \e[32m SUCCESS \e[0m"
+    else 
+        echo -e " \e[31m FAILURE \e[0m"
+    fi 
+}
+
+
+NODEJS() {
+    echo -n "Configuring NodeJS Repo: "
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+    stat $? 
+
+    echo -n "Installing NodeJS: "
+    yum install nodejs -y &>> $LOGFILE 
+    stat $? 
+}
