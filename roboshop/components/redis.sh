f62cd6d4b0ecd7f98b95bccd88c034b34ff43bd0
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:31:49 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -8,3 +8,6 @@ source components/common.sh
 
 # curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 # yum install redis-6.2.7 -y
+
+2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
+
