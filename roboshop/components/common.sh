e7dd41113c80aebf5e92254690cb1cfd0f0f358e
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:16:57 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -36,6 +36,19 @@ CREATE_USER() {
 }
 
 DOWNLOAD_AND_EXTRACT() {
+    echo -n "Downloading $COMPONENT repo: "
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $? 
 
+    echo -n "Performing cleanup: "
+    cd /home/roboshop/ && rm -rf ${COMPONENT}  &>> $LOGFILE 
+    stat $?
+
+    echo -n "Extracting $COMPONENT: "
+    cd /home/roboshop
+    unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE 
+    mv ${COMPONENT}-main ${COMPONENT}  &&  chown -R $APPUSER:$APPUSER $COMPONENT 
+    cd ${COMPONENT}
+    stat $?
 
 }
