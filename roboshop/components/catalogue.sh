1d80b55d35e41896b433f39177df049f14fe2665
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:04:29 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -30,7 +30,7 @@ stat $?
 echo -n "Extracting $COMPONENT: "
 cd /home/roboshop
 unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE 
-mv ${COMPONENT}-main ${COMPONENT}  &&  chown -R $APPUSER:$APPUSER $COMPONENT  ( chmod )
+mv ${COMPONENT}-main ${COMPONENT}  &&  chown -R $APPUSER:$APPUSER $COMPONENT 
 cd ${COMPONENT}
 stat $?
 
