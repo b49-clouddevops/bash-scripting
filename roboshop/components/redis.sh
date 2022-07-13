07d150b7c77ad3bdce883e8722b77979c4249168
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:38:28 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -16,6 +16,7 @@ stat $?
 
 echo -n "Updating the $COMPONENT listenting address: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>> $LOGFILE 
+stat $? 
 
 # 2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
 
