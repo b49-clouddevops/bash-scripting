0d780602b788f8658c27d3e6a8676ccbb761ad0c
mvpc-11232 <mvp2612@hotmail.com>
2022-07-13 08:34:44 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,11 +14,11 @@ echo -n "Installing $COMPONENT: "
 yum install redis-6.2.7 -y &>> $LOGFILE 
 stat $?
 
-2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
+# 2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
 
-# vim /etc/redis.conf
-# vim /etc/redis/redis.conf
+# # vim /etc/redis.conf
+# # vim /etc/redis/redis.conf
 
-# systemctl enable redis
-# systemctl start redis
-# systemctl status redis -l
+# # systemctl enable redis
+# # systemctl start redis
+# # systemctl status redis -l
