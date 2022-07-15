9feb703731b3f5b7dcf4c1671204857c6461edf3
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:51:05 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -85,7 +85,7 @@ DOWNLOAD_AND_EXTRACT() {
 
 CONFIG_SERVICE() {
     echo -n "Configuring $COMPONENT service: "
-    sed -i -e 's/CARTENDPOINT /cart.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' systemd.service
+    sed  -i -e 's/CARTENDPOINT /cart.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service  /etc/systemd/system/$COMPONENT.service
     stat $? 
 }
