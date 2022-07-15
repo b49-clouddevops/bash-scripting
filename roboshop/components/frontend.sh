ff97d8f08349b1cf769d96d01303458d04ebeb34
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:58:32 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -37,6 +37,6 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $? 
 
-
+echo -n "Configuring the proxy file: "
 
 echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
