aa72ffc82bd40ddb6af34f545ecdf77ad42fa698
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:47:20 +0530

    Auto Push Commit

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -7,6 +7,6 @@ APPUSER="roboshop"
 source components/common.sh
 
 # Calling MAVEN Function
-NODEJS 
+MAVEN
 
 echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
