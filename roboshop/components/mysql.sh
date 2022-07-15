1e5c406f618d9725b5a392362795113703aaf274
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:00:20 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -20,7 +20,7 @@ stat $?
 echo -n "Changing the default $COMPONENT root password: "
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpassword_change.sql
 DEFAULT_ROOT_PASSWORD=$(sudo grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
-mysql  ---uroot -p"$DEFAULT_ROOT_PASSWORD"  < /tmp/rootpassword_change.sql
+mysql  --connect-expired-password -uroot -p"$DEFAULT_ROOT_PASSWORD"  < /tmp/rootpassword_change.sql
 stat $? 
 
 
