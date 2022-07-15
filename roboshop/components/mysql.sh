6a30589a628f213acecb477e31943627a0630eed
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:16:37 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -29,8 +29,7 @@ fi
 
 # We need to handle this only for the first time
 echo "show plugins" | mysql -uroot -pRoboShop@1 &>> $LOGFILE | grep validate_password  &>> $LOGFILE 
-if [ $? -eq 0 ]; then 
-     
+if [ $? -eq 0 ]; then      
     echo -n "Uninstalling Password Validate Plugin: "
     echo 'uninstall plugin validate_password;' > /tmp/password-valudate.sql 
     mysql  --connect-expired-password -uroot -p"$DEFAULT_ROOT_PASSWORD"  &>> $LOGFILE 
