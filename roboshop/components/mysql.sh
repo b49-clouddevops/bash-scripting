2460364a9b17fa153c7cfe041da4cbf5ea30633e
mvpc-11232 <mvp2612@hotmail.com>
2022-07-15 08:16:05 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -33,6 +33,7 @@ if [ $? -eq 0 ]; then
      
     echo -n "Uninstalling Password Validate Plugin: "
     echo 'uninstall plugin validate_password;' > /tmp/password-valudate.sql 
+    mysql  --connect-expired-password -uroot -p"$DEFAULT_ROOT_PASSWORD" 
 
     echo -n "Changing the default $COMPONENT root password: "
     echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpassword_change.sql
