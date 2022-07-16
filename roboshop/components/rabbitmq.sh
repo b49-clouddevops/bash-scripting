80e799ec9c317fcf2920a141c51ef960564d86e1
mvpc-11232 <mvp2612@hotmail.com>
2022-07-16 07:49:25 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -10,7 +10,7 @@ echo -n "Configuring repo: "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $? 
 
-echo -n "Installing $PAYMENT Depenency Package Erlang"
+echo -n "Installing $PAYMENT Depenency Package Erlang and $COMPONEN"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>> $LOGFILE
 stat $? 
 
