61896affb04f2bcd72e87c21bcc89c183a01c78e
mvpc-11232 <mvp2612@hotmail.com>
2022-07-16 08:08:29 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -32,7 +32,7 @@ PYTHON(){
     echo -n "Updating the App Config $COMPONENT.ini"
     USER_ID=$(id -u roboshop)
     GROUP_ID=$(id -g roboshop)
-    sed -i -e "/uid/ c uid = $USER_ID"  -e "/gid/ c gid = $GROUP_ID" payment.ini
+    sed -i -e "/uid/ c uid = $USER_ID"  -e "/gid/ c gid = $GROUP_ID" $COMPONENT.ini
 
 }
 
