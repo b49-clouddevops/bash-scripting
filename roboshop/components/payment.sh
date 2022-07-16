e517cf552427450fb90abce518a5b719daa330e7
mvpc-11232 <mvp2612@hotmail.com>
2022-07-16 07:57:00 +0530

    Auto Push Commit

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -3,3 +3,10 @@ set -e
 COMPONENT=shipping 
 LOGFILE="/tmp/$COMPONENT.log"
 APPUSER="roboshop"
+
+source components/common.sh
+
+# Calling MAVEN Function
+MAVEN
+
+echo -e " ____________________ \e[32m $COMPONENT Configuration is completed ____________________ \e[0m"
