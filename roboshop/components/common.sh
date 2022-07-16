e786d65316cc32b469dfe50927ac22c4fdb5aff8
mvpc-11232 <mvp2612@hotmail.com>
2022-07-16 08:11:54 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -119,7 +119,7 @@ Update `AMQPHOST` with RabbitMQ server ip.
 
 CONFIG_SERVICE() {
     echo -n "Configuring $COMPONENT service: "
-    sed -i -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' systemd.service
+    sed -i -e 's/CARTHOST/mysql.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service  /etc/systemd/system/$COMPONENT.service
     stat $? 
 }
