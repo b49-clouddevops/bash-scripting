e3df8cee7ae0c9445bfa4198ad4f73fd5ea29166
mvpc-11232 <mvp2612@hotmail.com>
2022-07-16 07:46:13 +0530

    Auto Push Commit

---
+++ roboshop/components/rabbitmq.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+COMPONENT=mysql  
+LOGFILE="/tmp/$COMPONENT.log"
+source components/common.sh 
+APPUSER=roboshop
+
+
+echo -n "Configuring repo: "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
+stat $? 
+
+echo -n "Installing $PAYMENT Depenency Package Erlang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
+stat $? 
+
+echo -n "Installng $COMPONENT: "
+yum install rabbitmq-server -y &>> $LOGFILE
+stat $? 
+
+echo -n "Starting $COMPONENT: "
+systemctl enable rabbitmq-server &>> $LOGFILE 
+systemctl start rabbitmq-server &>> $LOGFILE
+stat $? 
+
+systemctl status rabbitmq-server -l &>> $LOGFILE
+stat $?
+
+echo -n "Creating $APPUSER user for rabbitmq: "
+rabbitmqctl add_user $APPUSER roboshop123
+stat $? 
+
+# rabbitmqctl set_user_tags $APPUSER administrator
+# rabbitmqctl set_permissions -p / $APPUSER ".*" ".*" ".*"
+
+# #We are good with rabbitmq.Next component is PAYMENT
