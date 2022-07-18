66d6498a9640550b56473a2f646f7a17a05326ec
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 08:16:59 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -10,7 +10,7 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabIma
 echo $AMI_ID 
 echo "$COMPONENT Server Creation in progress"
 
-PRIVATE_IP=${aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].'
+PRIVATE_IP=${aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress'
 }
 
 # Changing the IP Address and DNS Name as per the component
