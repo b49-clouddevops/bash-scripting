32b18cb9c7c99cd72adbecb3bdebfdc79cf62889
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 08:11:43 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -12,5 +12,5 @@ echo "$COMPONENT Server Creation in progress"
 aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq 
 
 # Changing the IP Address and DNS Name as per the component
-sed -e 's/IPADDRESS/${PRIVATE_IP}'/ - 
+sed -e 's/IPADDRESS/${PRIVATE_IP}'/ -e
 aws route53 change-resource-record-sets --hosted-zone-id Z09626353E72G6GNQ0R5A --change-batch file://sample.json
