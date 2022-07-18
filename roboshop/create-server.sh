181d5ed10aca012ed666e4bc5e9bbaa60b747dec
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 08:30:53 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -10,8 +10,6 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabIma
 echo $AMI_ID 
 echo "$COMPONENT Server Creation in progress"
 
-# PRIVATE_IP=${aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress' }
-
 PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 
