2668f3c6e5d305da732122f7fed5db05b2cccb71
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 07:38:56 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,5 +4,4 @@
 
 # AMI_ID="ami-0aa718de62aea6fbe"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-
 echo $AMI_ID 
