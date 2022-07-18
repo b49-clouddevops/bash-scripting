d99ce923cd0e6b449625abe4ca0cf23b590e0539
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 07:52:39 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -6,4 +6,4 @@
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
 echo "$COMPONENT Server Creation in progress"
-aws ec2 run-instances --image-id  $AMI_ID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]'  | jq 
+aws ec2 run-instances --image-id  $AMI_ID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=production}]'  | jq 
