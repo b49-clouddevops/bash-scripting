03df2ea7c264ba135c829a817f5970aaabb552f0
mvpc-11232 <mvp2612@hotmail.com>
2022-07-18 07:53:16 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -3,6 +3,8 @@
 # This script creates servers and the associated route53 internal hosted zone records.
 
 # AMI_ID="ami-0aa718de62aea6fbe"
+
+
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
 echo "$COMPONENT Server Creation in progress"
