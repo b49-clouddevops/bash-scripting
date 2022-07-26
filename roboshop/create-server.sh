f519d6d0c5affdeda6ffc7f32b1fb9702fff40a3
mvpc-11232 <mvp2612@hotmail.com>
2022-07-26 09:35:29 +0530

    Auto Push Commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -6,7 +6,7 @@
 # Disclaimer: This script works on CENTOS7 Only
 
 if [ "$1" = "" ] | [ "$2" = "" ] ; then 
-    echo -e "\e[31m Valid options are component -name or all and env \n: Ex: bash create-server.sh \e[0m "
+    echo -e "\e[31m Valid options are component -name or all and env \n: Ex: bash create-server.sh payment \e[0m "
     exit 1
 fi 
 
